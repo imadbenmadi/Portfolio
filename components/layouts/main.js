4bff92d12ad4a60ebc12994a05cfda2ab3f370bc
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-07 21:00:56 +0100

    .

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -11,14 +11,14 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Benmadi imed eddine Portfolio" />
         <meta name="author" content="Benmadi imed eddine" />
-        {/* <link rel="apple-touch-icon" href="apple-touch-icon.png" /> */}
-        {/* <link
+        <link rel="apple-touch-icon" href="code.png" />
+        <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/android-chrome-192x192.png"
-        ></link> */}
-        {/* <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> */}
+          href="/code.png"
+        ></link>
+        <link rel="shortcut icon" href="/code.png" type="image/x-icon" />
         <meta property="og:site_name" content="Benmadi imad" />
         <meta name="og:title" content="Benmadi imad" />
         <meta property="og:type" content="website" />
